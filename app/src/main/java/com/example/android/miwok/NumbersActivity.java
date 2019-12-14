e53baf5ab8037445c47c1970cdda50e79ff0e78a
DEVBRATDUBEY <devbratdubey3250@gmail.com>
2019-12-14 15:17:08 +0000

    success Multi screen v1

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+//import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -12,23 +14,24 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         // ceate an array of words
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
 
-        Log.v("NumbersActivity","words at index 0: " + words[0]);
-        Log.v("NumbersActivity","words at index 1: " + words[1]);
-        Log.v("NumbersActivity","words at index 2: " + words[2]);
-        Log.v("NumbersActivity","words at index 3: " + words[3]);
-        Log.v("NumbersActivity","words at index 4: " + words[4]);
-        Log.v("NumbersActivity","words at index 5: " + words[5]);
+
+//        Log.v("NumbersActivity","words at index 0: " + words.get(0));
+//        Log.v("NumbersActivity","words at index 1: " + words.get(1));
+//        Log.v("NumbersActivity","words at index 2: " + words.get(2));
+//        Log.v("NumbersActivity","words at index 3: " + words.get(3));
+//        Log.v("NumbersActivity","words at index 4: " + words.get(4));
+//        Log.v("NumbersActivity","words at index 5: " + words.get(5));
     }
 }
